b69bafa21e28f2f7776f886c65c7c9ca255f0ddd
maxingfang <maxingfang@meituan.com>
2023-06-06 14:56:54 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::io::{BufWriter, stdout};
+
+use ferris_says::say;
+
+fn main() {
+    let stdout = stdout();
+    let message = String::from("hello, rust");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
+
+    // println!("Hello, world!");
+}
