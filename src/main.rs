3e9f9451e957c404d9ad0c093b180c2eeece7329
maxingfang <maxingfang@meituan.com>
2023-06-13 21:34:52 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,40 @@
-use std::io::{BufWriter, stdout};
+use std::cmp::Ordering;
+use std::io;
 
 use ferris_says::say;
 
+use rand::Rng;
+
 fn main() {
-    let stdout = stdout();
+    let stdout = io::stdout();
     let message = String::from("hello, rust");
     let width = message.chars().count();
 
-    let mut writer = BufWriter::new(stdout.lock());
+    let mut writer = io::BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
 
     println!("Hello, world!");
+
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
